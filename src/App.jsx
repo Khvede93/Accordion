f6a181b6a39298c4af3c6316aeb1196288d42729
Khvede93 <gio.khvede93@gmail.com>
2024-09-08 17:29:49 +0200

    fix bug with Single selected answer display when switching to multiSelect

--- src/App.jsx
+++ src/App.jsx
@@ -11,40 +11,58 @@ function App() {
     setActive(active === getId ? null : getId);
   }
 
-  function handleMultiSelect(getId) {}
+  function handleEnableMultiSelection() {
+    setActive(null);
+    setEnableMultiSelection(!enableMultiSelection);
+  }
+
+  function handleMultiSelect(getId) {
+    const copyMultiple = [...multiple];
+    const findCurrent = copyMultiple.indexOf(getId);
+    if (findCurrent === -1) {
+      copyMultiple.push(getId);
+    } else {
+      copyMultiple.splice(findCurrent, 1);
+    }
+    setMultiple(copyMultiple);
+  }
 
   return (
     <div className='bg-[#20948b] h-screen flex justify-center'>
       <div className='w-full max-w-[25rem] mt-[2rem] flex flex-col'>
         <button
           className='p-3 bg-white mb-12 rounded-xl w-max self-end'
-          onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+          onClick={handleEnableMultiSelection}
         >
           {enableMultiSelection
             ? 'Disable Multi Selection'
             : 'Enable Multi Selection'}
         </button>
-        <div>
-          {data.map((QA) => (
-            <div key={QA.id} className=' flex flex-col mb-1'>
-              <div
-                onClick={
-                  enableMultiSelection
-                    ? () => handleMultiSelect(QA.id)
-                    : () => handleSingleSelect(QA.id)
-                }
-                className='flex cursor-pointer bg-white p-3 justify-between text-md font-medium'
-              >
-                <h2>{QA.question}</h2>
-                <span>{active === QA.id ? '-' : '+'}</span>
-              </div>
+        <section>
+          {data && data.length ? (
+            data.map((QA) => (
+              <div key={QA.id} className=' flex flex-col mb-1'>
+                <div
+                  onClick={
+                    enableMultiSelection
+                      ? () => handleMultiSelect(QA.id)
+                      : () => handleSingleSelect(QA.id)
+                  }
+                  className='flex cursor-pointer bg-white p-3 justify-between text-md font-medium'
+                >
+                  <h2>{QA.question}</h2>
+                  <span>{active === QA.id ? '-' : '+'}</span>
+                </div>
 
-              {active === QA.id ? (
-                <p className='bg-[#1db1a6] border-t p-3 pb-10'>{QA.answer}</p>
-              ) : null}
-            </div>
-          ))}
-        </div>
+                {active === QA.id ? (
+                  <p className='bg-[#1db1a6] border-t p-3 pb-10'>{QA.answer}</p>
+                ) : null}
+              </div>
+            ))
+          ) : (
+            <div>No data found !!!</div>
+          )}
+        </section>
       </div>
     </div>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -1,24 +1,24 @@
 const data = [
   {
-    id: '1',
+    id: 1,
     question: 'What is the Hubble Space Telescope?',
     answer:
       'The Hubble Space Telescope is a large space telescope that orbits Earth. It has provided some of the most detailed images of distant galaxies, nebulae, and other celestial objects, greatly enhancing our understanding of the universe.',
   },
   {
-    id: '2',
+    id: 2,
     question: 'What is a black hole?',
     answer:
       'A black hole is a region of space where gravity is so strong that nothing, not even light, can escape from it. They are formed from the remnants of massive stars that collapse under their own gravity after their nuclear fuel is exhausted.',
   },
   {
-    id: '3',
+    id: 3,
     question: 'What is the International Space Station (ISS)?',
     answer:
       'The International Space Station is a large spacecraft in low Earth orbit that serves as a microgravity laboratory. It is a joint project involving NASA, Roscosmos, ESA, JAXA, and CSA, where astronauts conduct scientific research and technology demonstrations.',
   },
   {
-    id: '4',
+    id: 4,
     question: 'What is dark matter?',
     answer:
       'Dark matter is a form of matter that does not emit, absorb, or reflect light, making it invisible and detectable only through its gravitational effects. It is thought to make up about 27% of the universe and plays a crucial role in the formation and structure of galaxies.',
