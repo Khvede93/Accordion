d4bba5358d8643a97bb5e2476014376872825f7c
Khvede93 <gio.khvede93@gmail.com>
2024-09-08 16:59:04 +0200

    enable multiselection button

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,47 @@ import data from './data/data';
 
 function App() {
   const [active, setActive] = useState(null);
-  const [enableMultiSelection, setEnableMultiSelection] = useState(true);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
   const [multiple, setMultiple] = useState([]);
 
   function handleSingleSelect(getId) {
     setActive(active === getId ? null : getId);
   }
 
-  function handleMultiSelect(getId) {
-    console.log('test' + getId);
-  }
+  function handleMultiSelect(getId) {}
 
   return (
     <div className='bg-[#20948b] h-screen flex justify-center'>
-      <div className='w-full max-w-[25rem] mt-[2rem]'>
-        {data.map((QA) => (
-          <div key={QA.id} className=' flex flex-col mb-1'>
-            <div
-              onClick={
-                enableMultiSelection
-                  ? () => handleMultiSelect(QA.id)
-                  : () => handleSingleSelect(QA.id)
-              }
-              className='flex cursor-pointer bg-white p-3 justify-between text-md font-medium'
-            >
-              <h2>{QA.question}</h2>
-              <span>{active === QA.id ? '-' : '+'}</span>
-            </div>
+      <div className='w-full max-w-[25rem] mt-[2rem] flex flex-col'>
+        <button
+          className='p-3 bg-white mb-12 rounded-xl w-max self-end'
+          onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+        >
+          {enableMultiSelection
+            ? 'Disable Multi Selection'
+            : 'Enable Multi Selection'}
+        </button>
+        <div>
+          {data.map((QA) => (
+            <div key={QA.id} className=' flex flex-col mb-1'>
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelect(QA.id)
+                    : () => handleSingleSelect(QA.id)
+                }
+                className='flex cursor-pointer bg-white p-3 justify-between text-md font-medium'
+              >
+                <h2>{QA.question}</h2>
+                <span>{active === QA.id ? '-' : '+'}</span>
+              </div>
 
-            {active === QA.id ? (
-              <p className='bg-[#1db1a6] border-t p-3 pb-10'>{QA.answer}</p>
-            ) : null}
-          </div>
-        ))}
+              {active === QA.id ? (
+                <p className='bg-[#1db1a6] border-t p-3 pb-10'>{QA.answer}</p>
+              ) : null}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
