56c20fd1a26f6c181eac23afa26c2664bb0e7760
Khvede93 <gio.khvede93@gmail.com>
2024-09-08 15:14:51 +0200

    temp working version of singleSelect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import { useState } from 'react';
 import './App.css';
+import data from './data/data';
 
 function App() {
-  return <div className='bg-red-400'>Accordion</div>;
+  const [active, setActive] = useState(null);
+  function handleSingleSelect(getId) {
+    active === getId ? setActive(null) : setActive(getId);
+  }
+  console.log(active);
+
+  return (
+    <div>
+      {data.map((QA) => (
+        <div key={QA.id} className='w-[400px] flex flex-col gap-1'>
+          <h2 onClick={() => handleSingleSelect(QA.id)}>{QA.question}</h2>
+          {active === QA.id ? <p className='mb-[1rem]'>{QA.answer}</p> : null}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
